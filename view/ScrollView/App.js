bbc9075aa6238cac688839c947f7804de5f1f837
TitanLi <Lisheng0706@gmail.com>
2018-07-11 10:40:14 +0800

    Add view scrollView

---
+++ view/ScrollView/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, TextInput, ScrollView, Image, View } from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+      <ScrollView>
+        <Text style={{ fontSize: 96 }}>Scroll me plz</Text>
+        <Image source={require('./img/download.jpeg')} />
+        <Image source={require('./img/download.jpeg')} />
+        <Image source={require('./img/download.jpeg')} />
+        <Image source={require('./img/download.jpeg')} />
+        <Text style={{ fontSize: 96 }}>If you like</Text>
+        <Image source={require('./img/download.jpeg')} />
+        <Image source={require('./img/download.jpeg')} />
+        <Image source={require('./img/download.jpeg')} />
+        <Image source={require('./img/download.jpeg')} />
+        <Text style={{ fontSize: 96 }}>Scrolling down</Text>
+        <Image source={require('./img/download.jpeg')} />
+        <Image source={require('./img/download.jpeg')} />
+        <Image source={require('./img/download.jpeg')} />
+        <Image source={require('./img/download.jpeg')} />
+        <Text style={{ fontSize: 96 }}>What's the best</Text>
+        <Image source={require('./img/download.jpeg')} />
+        <Image source={require('./img/download.jpeg')} />
+        <Image source={require('./img/download.jpeg')} />
+        <Image source={require('./img/download.jpeg')} />
+        <Text style={{ fontSize: 96 }}>Framework around?</Text>
+        <Image source={require('./img/download.jpeg')} />
+        <Image source={require('./img/download.jpeg')} />
+        <Image source={require('./img/download.jpeg')} />
+        <Image source={require('./img/download.jpeg')} />
+        <Text style={{ fontSize: 80 }}>React Native</Text>
+      </ScrollView>
+    )
+  }
+}
